3366828d42635a7c44bc7cce24ead6aec780bc80
kai <kai810215@gmail.com>
2025-06-25 23:36:15 +0800

    Initial Commit

---
+++ Gudi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gudi
+//
+//  Created by 林聖凱 on 2025/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
